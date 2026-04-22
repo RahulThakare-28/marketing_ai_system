bc9f038215bb91761cde06db6b3cf826de85793a
RahulThakare-28 <rahulthakare6444@gmail.com>
2026-04-22 18:44:08 +0530

    update to handle beater error, or make debug easy

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,5 +1,5 @@
 
-
+'''
 import joblib
 from src.utils.logger import get_logger
 
@@ -9,6 +9,36 @@ class Predictor:
 
     def __init__(self):
         self.model = joblib.load("src/models/model.pkl")
+
+    def predict(self, df):
+        try:
+            X = df[["total_score", "total_amount", "price"]]
+            df["probability"] = self.model.predict_proba(X)[:, 1]
+            return df
+
+        except Exception as e:
+            logger.error(f"Prediction error: {e}")
+            raise
+'''
+import os
+import joblib
+from src.utils.logger import get_logger
+
+logger = get_logger("Predict")
+
+class Predictor:
+
+    def __init__(self):
+        try:
+            base_path = os.path.dirname(os.path.abspath(__file__))
+            model_path = os.path.join(base_path, "model.pkl")
+
+            self.model = joblib.load(model_path)
+            logger.info("Model loaded successfully")
+
+        except Exception as e:
+            logger.error(f"Model loading failed: {e}")
+            raise
 
     def predict(self, df):
         try:
