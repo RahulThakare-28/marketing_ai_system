34fa5edb907985af368b262f8613170f7929fbd9
RahulThakare-28 <rahulthakare6444@gmail.com>
2026-04-22 19:53:52 +0530

    update for , filter data before prediction

--- src/pipeline/main_pipeline.py
+++ src/pipeline/main_pipeline.py
@@ -8,9 +8,31 @@ from src.targeting.filter import TargetSelector
 
 class Pipeline:
 
-    def run(self):
+    def run(self, product_input=None):
         loader = DataLoader()
         data = loader.load_all()
+        # new integrate code for filtering
+        product_df = data["products"]
+
+        if product_input:
+            name = product_input.get("product_name", "")
+            category = product_input.get("category", "")
+            brand = product_input.get("brand", "")
+
+            if name:
+                product_df = product_df[
+                product_df["product_name"].str.contains(name, case=False, na=False)
+                ]
+
+            if category:
+                product_df = product_df[
+                product_df["category"] == category
+                ]
+
+            if brand:
+                product_df = product_df[
+                product_df["brand"].str.contains(brand, case=False, na=False)
+            ]
 
         merger = DataMerger()
         merged = merger.merge(data)
@@ -19,6 +41,12 @@ class Pipeline:
         interaction = interaction_engine.compute_score(
             data["events"], data["reviews"]
         )
+ 
+        # new integrade for product base filter
+        product_ids = product_df["product_id"].unique()
+        interaction = interaction[
+            interaction["product_id"].isin(product_ids)
+        ]
 
         feature_builder = FeatureBuilder()
         features = feature_builder.build(merged, interaction)
@@ -29,4 +57,10 @@ class Pipeline:
         selector = TargetSelector()
         result = selector.select(predictions)
 
+        # new integrate 
+        users = data["users"]
+        result = result.merge(users, on="user_id", how="left")
         return result
+       
+
+        
